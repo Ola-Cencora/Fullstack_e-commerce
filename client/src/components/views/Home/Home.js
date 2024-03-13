53d89ef4ef3ee13c5b79665be5eac5c12857661b
Ola <cencora.aleksandra@gmail.com>
2024-03-13 16:47:31 +0100

    Add redirect boxes to home page

--- client/src/components/views/Home/Home.js
+++ client/src/components/views/Home/Home.js
@@ -1,8 +1,13 @@
-const Home = () => {
-    //list of categories, when clicked moving to CategoryPage
-    return (
-        <p>Home</p>
-    );
-};
+import RedirectBox from '../../common/RedirectBox/RedirectBox';
+import styles from './Home.module.scss';
+
+const Home = () => (
+  <section className={styles.home}>
+    <RedirectBox category="all" text="wszystkie" img="all.jpg" />
+    <RedirectBox category="mugs" text="kubki" img="mugs.jpg" />
+    <RedirectBox category="bowls" text="miski" img="bowls.jpg" />
+    <RedirectBox category="vases" text="wazony" img="vases.jpg" />
+  </section>
+);
 
 export default Home;
